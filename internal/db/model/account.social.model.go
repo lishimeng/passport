9ba096014753ee7d8e186144e053ada2367d2a68
alex.dou <alex.dou@qq.com>
2023-06-20 19:48:56 +0800

    c

--- internal/db/model/account.social.model.go
+++ internal/db/model/account.social.model.go
@@ -8,7 +8,7 @@ type SocialAccount struct {
 	AccountId       int            `orm:"column(account_id);null"`
 	SocialAccountId string         `orm:"column(social_account_id);null"` // 第三方账号的key
 	SocialGroupId   string         `orm:"column(social_group_id);null"`   // 对应微信union id,如没有可置空
-	Category        SocialCategory `json:"column(category);null"`
+	Category        SocialCategory `orm:"column(category);null"`
 	app.TableChangeInfo
 }
 
